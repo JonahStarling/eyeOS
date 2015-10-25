002b1d860515925eebdb4153f8aa44361d3aca21
JonahStarling <starling.jonah@gmail.com>
2015-10-24 20:48:18 -0400

    Moving More Files Around Getting Set Up
    Moved files around again to try and make this as simple and
    uncomplicated as possible. Added eye tracking code.

---
+++ easyKeyboard/src/ofApp.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ofMain.h"
+#include "EyeButton.h"
+#include "EyeCursor.h"
+#include "../stdafx.h" //Not found yet
+#include "../../include/eyex/EyeX.h"
+#include "eyeXHost.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        int menuMain(int i);
+        void menuABC(int i);
+        void menuSuggestions(int i );
+        void menuHIJ(int i);
+        void menuOPQ(int i);
+        void menuUVW(int i);
+        void findSuggestions();
+    
+        eyeXHost eyex;
+};
--- easyKeyboard_WIN/src/EyeButtonGrid.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  EyeButtonGrid.cpp
-//  easyKeyboard
-//
-//  Created by Jonah Starling on 10/21/15.
-//
-//
-
-#include "EyeButtonGrid.h"
